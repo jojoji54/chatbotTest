beb98609ad8b3c5621b9bb183d00c7a6d68452c4
jojoji54 <35396962+jojoji54@users.noreply.github.com>
2021-04-01 23:03:11 +0200

    cors added

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
 from flask import Flask, jsonify,request
+from flask_cors import CORS
 import time
 app = Flask(__name__);
+CORS(app)
 @app.route("/bot", methods=["POST"])
+
+#response
 def response():
     query = dict(request.form)['query']
-    res = query + " " + time.ctime()
+    res = query + " " 
     return jsonify({"response" : res})
+     
 if __name__=="__main__":
     app.run(host="0.0.0.0",)
