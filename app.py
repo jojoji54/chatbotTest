71c226d4a1edf7fff668cb59030c50b007f3cddd
jojoji54 <35396962+jojoji54@users.noreply.github.com>
2021-04-22 19:10:21 +0200

    z<

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, jsonify,request
+from flask_cors import CORS
+import time
+app = Flask(__name__);
+CORS(app)
+@app.route("/bot", methods=["POST"])
+
+#response
+def response():
+    query = dict(request.form)['query']
+    res = query + " " 
+    return jsonify({"response" : res})
+     
+if __name__=="__main__":
+    app.run(host="0.0.0.0",) 
+    #app.run(debug = True,port = 8080)
