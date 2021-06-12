52944cc4684a8116bda1e4584fd43ad0c37995a9
jojoji54 <35396962+jojoji54@users.noreply.github.com>
2021-06-13 00:28:34 +0200

    Update app.py

--- app.py
+++ app.py
@@ -70,10 +70,10 @@ def response():
         for intent in intents['intents']:
             if tag == intent["tag"]:
                 if intent["tag"] == "goodbye":
-                    os.system('python database.py')
-                    os.system('python randomDatabase.py')
+                   # os.system('python database.py')
+                   #os.system('python randomDatabase.py')
                    # os.system('python train.py')
-                    return jsonify({"response": random.choice(intent['responses'])}) & os.system('python train.py')
+                    return jsonify({"response": "See you :)"}) & os.system('python train.py')
                 # elif intent["tag"] == "goodbye":
                 #      os.system('python train.py')
                 #      return jsonify({"response" : train.epoch})
