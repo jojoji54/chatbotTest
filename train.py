e7b7f2420e14a6ab4a57e610847294a246149aba
jojoji54 <35396962+jojoji54@users.noreply.github.com>
2021-06-12 23:21:22 +0200

    Update train.py

--- train.py
+++ train.py
@@ -10,8 +10,8 @@ from torch.utils.data import Dataset, DataLoader
 from nltk_utils import bag_of_words, tokenize, stem
 from model import NeuralNet
 
-# os.system('python database.py')
-# os.system('python randomDatabase.py')
+os.system('python database.py')
+os.system('python randomDatabase.py')
 #global epoch
 
 with open('intents.json', 'r') as f:
