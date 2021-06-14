ddd775fd1c342132fcf0d93acf1f41b56cf469c6
jojoji54 <35396962+jojoji54@users.noreply.github.com>
2021-06-14 21:16:40 +0200

    tes final 1

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+
 import random
 import json
 from flask import Flask, jsonify, request
@@ -64,7 +65,7 @@ def response():
     probs = torch.softmax(output, dim=1)
     prob = probs[0][predicted.item()]
 
-    if prob.item() > 0.60:
+    if prob.item() > 0.70:
         app.logger.info('%d logged in successfully', prob.item())
         app.logger.info(intents['intents'])
         for intent in intents['intents']:
@@ -79,6 +80,10 @@ def response():
                 #      return jsonify({"response" : train.epoch})
                 else:
                     return jsonify({"response": random.choice(intent['responses'])})
+    elif prob.item() > 0.50 < 0.70:
+        app.logger.info('%d logged in successfully', prob.item())
+        app.logger.info(intents['intents'])
+        return jsonify({"response": random.choice(['I siee...', 'mmmmmm', 'ops..', 'O_O'])}) 
 
     else:
         return jsonify({"response": "..."})
