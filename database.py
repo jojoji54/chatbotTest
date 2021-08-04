f9838421eddafacf6286723f3050637565329779
jojoji54 <35396962+jojoji54@users.noreply.github.com>
2021-08-04 02:03:13 +0200

    Add files via upload

--- database.py
+++ database.py
@@ -12,9 +12,15 @@ db = firestore.client()
 # read data
 # Getting a Document with a known ID
 
-intents = {"intents": []}
-detect_duplicate_by_tag = []
+#Para hacer mas inteligente nuestra IA tenemos que guardar dos tipos de parametros , los que van a ser usador para pregunta y los que no van a ser u
+#usados para nada , sin embargo servira para agrandar la base de datos de la IA, este archivo recupera los datos que  serviran
+#para preguntas
 
+intents = {"intents": []} #etiqueta del archivo json
+detect_duplicate_by_tag = [] #variable que se usara para no duplicar entras
+
+
+#Esto de abajo es una condicion la cual me permitira , no guardar archivos duplicados
 with open('intents.json') as json_file:
     olddata = json.load(json_file)
     for ob in olddata['intents']:
@@ -22,6 +28,7 @@ with open('intents.json') as json_file:
             intents['intents'].append(ob)
             detect_duplicate_by_tag.append(ob["tag"])
 
+#Esto de qui abajo, me permita recuperar los datos de la base de datos que tienen la etiqueta de la condicion
 print("old data:", len(intents['intents']))
 results = db.collection('users').document(
     'Peo5kqpi4GORXehD3oQVRXpHGfD2').collection('chats').get()
@@ -35,6 +42,7 @@ for index, result in enumerate(results):
         })
         detect_duplicate_by_tag.append(f"firebaseData{index}")
 
+#Una vez obtenidos los datos, y cmprobados los datos a no duplicar, lo guardamos en el archivo json
 print("new data: ", len(intents['intents']))
 with open("intents.json", "w") as outfile:
     json.dump(intents, outfile)
