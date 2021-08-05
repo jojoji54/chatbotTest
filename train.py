6f7924f5ce5ca740b647de7b4be17e78e8264881
jojoji54 <35396962+jojoji54@users.noreply.github.com>
2021-08-05 22:48:13 +0200

    sws
    dw

--- train.py
+++ train.py
@@ -82,7 +82,7 @@ class ChatDataset(Dataset):
     def __getitem__(self, index):
         return self.x_data[index], self.y_data[index]
 
-    # Podemos llamar a la longitud del dataset para calcular el tamaño
+    # Podemos llamar a la longitud del dataset para calcular el tamaños
     def __len__(self):
         return self.n_samples
 
