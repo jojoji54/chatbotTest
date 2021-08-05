e8491dd3d8ad071fc17ae2fb63bb464397ae7678
jojoji54 <35396962+jojoji54@users.noreply.github.com>
2021-08-05 21:48:14 +0200

    Update train.py

--- train.py
+++ train.py
@@ -64,10 +64,10 @@ y_train = np.array(y_train)
 
 # hiperparámetros
 num_epochs = 3000
-batch_size = 8
+batch_size = 38
 learning_rate = 0.001
 input_size = len(X_train[0])
-hidden_size = 8
+hidden_size = 38
 output_size = len(tags)
 print(input_size, output_size)
 
