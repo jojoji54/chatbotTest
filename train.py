a3079cf9ff4a0d0314f9a008f7a1f3d2251d28dd
jojoji54 <35396962+jojoji54@users.noreply.github.com>
2021-08-07 14:46:28 +0200

    Update train.py

--- train.py
+++ train.py
@@ -67,7 +67,7 @@ num_epochs = 1500
 batch_size = 1
 learning_rate = 0.001
 input_size = len(X_train[0])
-hidden_size6 = 10
+hidden_size6 = 18 #en 10 esta bastante bien
 hidden_size = 8
 output_size = len(tags)
 print(input_size, output_size)
