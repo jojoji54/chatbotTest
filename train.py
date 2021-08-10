5d902480251203030fda96c16d922ccfa5fd6625
jojoji54 <35396962+jojoji54@users.noreply.github.com>
2021-08-11 00:32:02 +0200

    Update train.py

--- train.py
+++ train.py
@@ -68,7 +68,7 @@ y_train = np.array(y_train)
 
 # hiperparámetros
 num_epochs = 8000  # en 6000 funciona muy bien
-batch_size = 512  # en 128 Funciona muy bien
+batch_size = 32  # en 128 Funciona muy bien
 learning_rate = 0.001
 input_size = len(X_train[0])
 hidden_size6 = 16  # en 10 y 8 esta bastante bien
