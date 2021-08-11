4e4f4129342cd937e4f9569d32867361a5697334
jojoji54 <35396962+jojoji54@users.noreply.github.com>
2021-08-11 12:23:40 +0200

    Add files via upload

--- train.py
+++ train.py
@@ -63,12 +63,14 @@ X_train = np.array(X_train)
 y_train = np.array(y_train)
 
 # hiperparámetros
-num_epochs = 6000 #en 6000 funciona muy bien
-batch_size = 128 # en 128 Funciona muy bien
+num_epochs = 8000 #en 6000 funciona muy bien
+batch_size = 256 # en 128 Funciona muy bien
 learning_rate = 0.001
 input_size = len(X_train[0])
-hidden_size6 = 32 #en 10 y 8 esta bastante bien
-hidden_size = 8 #en 8 esta bastante bien
+hidden_size6 = 64 #en 10 y 8 esta bastante bien
+hidden_size3 = 32
+hidden_size2 = 16
+hidden_size = 16 #en 8 esta bastante bien
 output_size = len(tags)
 print(input_size, output_size)
 
@@ -95,7 +97,7 @@ train_loader = DataLoader(dataset=dataset,
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-model = NeuralNet(input_size, hidden_size6,hidden_size, output_size).to(device)
+model = NeuralNet(input_size, hidden_size6,hidden_size3,hidden_size2,hidden_size, output_size).to(device)
 
 # Loss y el optimizador
 criterion = nn.CrossEntropyLoss()
@@ -129,6 +131,8 @@ data = {
 "model_state": model.state_dict(),
 "input_size": input_size,
 "hidden_size6": hidden_size6,
+"hidden_size3": hidden_size3,
+"hidden_size2": hidden_size2,
 "hidden_size": hidden_size,
 "output_size": output_size,
 "all_words": all_words,
